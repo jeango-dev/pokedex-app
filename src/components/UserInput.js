a6ac3a0915f69e8bd200b5143383e49d978bf499
John Doe <johndoe@example.com>
2022-07-21 23:52:12 -0500

    change buttons pokedex

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,22 +1,22 @@
 import '../styles/userInput.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { changeUser } from '../store/slices/user.slice';
 import { useDispatch } from 'react-redux';
 import pokemons from '../img/pokemons.png';
-// import LoadingScreen from './LoadingScreen';
+import LoadingScreen from './LoadingScreen';
 
 const PokemonInput = () => {
   const [userName, setUserName] = useState('');
-  // const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  // useEffect(() => {
-  //   setLoading(false);
-  //   setTimeout(() => setLoading(), 1200);
-  // }, []);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => setLoading(), 1200);
+  }, []);
 
   const getName = () => {
     dispatch(changeUser(userName));
@@ -25,38 +25,38 @@ const PokemonInput = () => {
 
   return (
     <div>
-      {/* {loading ? ( */}
-      {/* <LoadingScreen /> */}
-      {/* ) : ( */}
-      <div className="container-user text-center align-items-center justify-content-center">
-        <div className="col">
-          <div>
-            <img className="user-image" src={pokemons} alt="" />
+      {loading ? (
+        <LoadingScreen />
+      ) : (
+        <div className="container-user text-center align-items-center justify-content-center">
+          <div className="col">
+            <div>
+              <img className="user-image" src={pokemons} alt="" />
+            </div>
+            <img
+              className="user-gif"
+              src="https://sergiofrancodev.com/pokeball.gif"
+              alt=""
+            />
+            <h2 className="title-trainer">Hello Master Pokemon</h2>
+            <input
+              style={{ backgroundColor: '#dc3545', color: 'white' }}
+              className="user-input"
+              type="text"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+              placeholder="Enter your name"
+            />
+            <button
+              style={{ backgroundColor: '#dc3545', color: 'white' }}
+              className="user-btn"
+              onClick={getName}
+            >
+              <i className="fas fa-xl fa-paper-plane"></i>
+            </button>
           </div>
-          <img
-            className="user-gif"
-            src="https://sergiofrancodev.com/pokeball.gif"
-            alt=""
-          />
-          <h2 className="title-trainer">Hello Master Pokemon</h2>
-          <input
-            style={{ backgroundColor: '#dc3545', color: 'white' }}
-            className="user-input"
-            type="text"
-            value={userName}
-            onChange={(e) => setUserName(e.target.value)}
-            placeholder="Enter your name"
-          />
-          <button
-            style={{ backgroundColor: '#dc3545', color: 'white' }}
-            className="user-btn"
-            onClick={getName}
-          >
-            <i className="fas fa-xl fa-paper-plane"></i>
-          </button>
         </div>
-      </div>
-      {/* )} */}
+      )}
     </div>
   );
 };
