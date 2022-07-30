07a57fabda51374b2da4fb9e7358ec05951e1f16
John Doe <johndoe@example.com>
2022-07-29 19:50:21 -0500

    change colors card

--- src/components/ColorCard.js
+++ src/components/ColorCard.js
@@ -1,38 +1,38 @@
 const ColorCard = (type) => {
   if (type === 'normal' || type === 1) {
-    return '#a8a878';
+    return '#9499a4';
   } else if (type === 'fighting' || type === 2) {
-    return '#c02038';
+    return '#db405c';
   } else if (type === 'flying' || type === 3) {
-    return '#a890f0';
+    return '#99b6e0';
   } else if (type === 'poison' || type === 4) {
-    return '#a040a0';
+    return '#ae64cd';
   } else if (type === 'ground' || type === 5) {
-    return '#e0c068';
+    return '#dd7f51';
   } else if (type === 'rock' || type === 6) {
     return '#b8a038';
   } else if (type === 'bug' || type === 7) {
-    return '#a8b820';
+    return '#9cc132';
   } else if (type === 'ghost' || type === 8) {
     return '#705898';
   } else if (type === 'steel' || type === 9) {
-    return '#ababc7';
+    return '#518b9e';
   } else if (type === 'fire' || type === 10) {
-    return '#f08030';
+    return '#f99e4b';
   } else if (type === 'water' || type === 11) {
-    return '#6890f0';
+    return '#4995eb';
   } else if (type === 'grass' || type === 12) {
-    return '#78c850';
+    return '#62bc5c';
   } else if (type === 'electric' || type === 13) {
     return '#f8d030';
   } else if (type === 'psychic' || type === 14) {
     return '#f85888';
   } else if (type === 'ice' || type === 15) {
-    return '#98d8d8';
+    return '#80d1c8';
   } else if (type === 'dragon' || type === 16) {
-    return '#7038f8';
+    return '#0771c5';
   } else if (type === 'dark' || type === 17) {
-    return '#705848';
+    return '#5f5c69';
   } else if (type === 'fairy' || type === 18) {
     return '#ee99ac';
   } else if (type === 'shadow' || type === 19) {
